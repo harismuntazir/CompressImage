5d35cc211214a1be4158487e3c1e999b81debac8
hm <haris.0813.m@gmail.com>
2023-07-29 06:44:40 +0530

    Update Image Compression.py

--- Image Compression.py
+++ Image Compression.py
@@ -1,13 +1,13 @@
-from PIL import Image
+# imgcom.py
 import os
+from PIL import Image
 
 def compress_image(image_path, quality, max_size):
     image = Image.open(image_path).convert("RGB")
     output_path = "out_" + image_path
     
-    # Scale down the image until it's under the max_size
     while image.size[0] > 1:
-        image.thumbnail((image.size[0]*0.9, image.size[1]*0.9))  # Reduce size by 10%
+        image.thumbnail((image.size[0]*0.9, image.size[1]*0.9))
         image.save(output_path, "JPEG", quality=quality)
         
         if os.stat(output_path).st_size / 1024 <= max_size:
@@ -15,11 +15,15 @@ def compress_image(image_path, quality, max_size):
 
     return output_path
 
-file_name = input("Enter the image file name: ")
-compress_upto = int(input("Enter the compression quality (1-100): "))
-max_size = float(input("Enter the desired maximum size in KB: "))
-output_file = compress_image(file_name, compress_upto, max_size)
+def main():
+    file_name = input("Enter the image file name: ")
+    compress_upto = int(input("Enter the compression quality (1-100): "))
+    max_size = float(input("Enter the desired maximum size in KB: "))
+    output_file = compress_image(file_name, compress_upto, max_size)
 
-size = os.stat(output_file).st_size / 1024
-print(f"The output file size is: {size} KB")
-print("The compression completed successfully.")
+    size = os.stat(output_file).st_size / 1024
+    print(f"The output file size is: {size} KB")
+    print("The compression completed successfully.")
+
+if __name__ == "__main__":
+    main()
